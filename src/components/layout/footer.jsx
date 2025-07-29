ce7b078328075204296dc91569081e7931023301
AnhKhoaDev <nguyenanhkhoa.9a3.tt@gmail.com>
2025-07-29 14:40:53 +0700

    video 68

---
+++ src/components/layout/footer.jsx
@@ -0,0 +1,12 @@
+import './footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>ReactVite - @AnhKhoa</p>
+        </div>
+
+    )
+}
+
+export default Footer;
---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,5 @@
+const MyComponent = () => {
+  return <div>eric & hoidanit</div>;
+};
+
+export default MyComponent();
