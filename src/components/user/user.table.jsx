33c0bc340571ad72322b027b003a55d1cf6ac012
AnhKhoaDev <nguyenanhkhoa.9a3.tt@gmail.com>
2025-08-20 15:20:39 +0700

    user

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,81 +1,113 @@
-import { Space, Table, Tag } from 'antd';
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
+import { Table, Popconfirm, notification } from 'antd';
+import UpdateUserModal from './update.user.modal';
+import { useState } from 'react';
+import ViewUserDetail from './view.user.detail';
+import { deleteUserAPI } from '../../services/api.service';
+
+const UserTable = (props) => {
+    const { dataUsers, loadUser } = props;
+
+    const [isModalUpdateOpen, setIsModalUpdateOpen] = useState(false);
+    const [dataUpdate, setDataUpdate] = useState(null);
 
 
-const UserTable = () => {
+    const [isDetailOpen, setIsDetailOpen] = useState(false);
+    const [dataDetail, setDataDetail] = useState(null);
+
+    const handleDeleteUser = async (id) => {
+        const res = await deleteUserAPI(id);
+
+        if (res.data) {
+            notification.success({
+                message: "Delete user",
+                description: "Xoá user thành công",
+            })
+            await loadUser();
+        } else {
+            notification.error({
+                message: "Error delete user",
+                description: JSON.stringify(res.message)
+            })
+        }
+    }
+
     const columns = [
         {
-            title: 'Name',
-            dataIndex: 'name',
-            key: 'name',
-            render: text => <a>{text}</a>,
+            title: 'Id',
+            dataIndex: '_id',
+            render: (_, record) => {
+                return (
+                    <a
+                        href='#'
+                        onClick={() => {
+                            setDataDetail(record);
+                            setIsDetailOpen(true);
+                        }}
+                    >
+                        {record._id}
+                    </a>
+                )
+            },
         },
         {
-            title: 'Age',
-            dataIndex: 'age',
-            key: 'age',
+            title: 'Full Name',
+            dataIndex: 'fullName',
         },
         {
-            title: 'Address',
-            dataIndex: 'address',
-            key: 'address',
-        },
-        {
-            title: 'Tags',
-            key: 'tags',
-            dataIndex: 'tags',
-            render: (_, { tags }) => (
-                <>
-                    {tags.map(tag => {
-                        let color = tag.length > 5 ? 'geekblue' : 'green';
-                        if (tag === 'loser') {
-                            color = 'volcano';
-                        }
-                        return (
-                            <Tag color={color} key={tag}>
-                                {tag.toUpperCase()}
-                            </Tag>
-                        );
-                    })}
-                </>
-            ),
+            title: 'Email',
+            dataIndex: 'email',
         },
         {
             title: 'Action',
             key: 'action',
             render: (_, record) => (
-                <Space size="middle">
-                    <a>Invite {record.name}</a>
-                    <a>Delete</a>
-                </Space>
+                <div style={{ display: "flex", gap: "20px" }}>
+                    <EditOutlined
+                        style={{ cursor: "pointer", color: "orange" }}
+                        onClick={() => {
+                            setDataUpdate(record);
+                            setIsModalUpdateOpen(true);
+                        }}
+                    />
+                    <Popconfirm
+                        title="Xoá người dùng"
+                        description="Bạn chắc chắn xoá user này?"
+                        onConfirm={() => handleDeleteUser(record._id)}
+                        okText="Yes"
+                        cancelText="No"
+                        placement="left"
+                    >
+                        <DeleteOutlined style={{ cursor: "pointer", color: "red" }} />
+                    </Popconfirm>
+                </div>
             ),
-        },
-    ];
-    const data = [
-        {
-            key: '1',
-            name: 'John Brown',
-            age: 32,
-            address: 'New York No. 1 Lake Park',
-            tags: ['nice', 'developer'],
-        },
-        {
-            key: '2',
-            name: 'Jim Green',
-            age: 42,
-            address: 'London No. 1 Lake Park',
-            tags: ['loser'],
-        },
-        {
-            key: '3',
-            name: 'Joe Black',
-            age: 32,
-            address: 'Sydney No. 1 Lake Park',
-            tags: ['cool', 'teacher'],
         },
     ];
 
     return (
-        <Table columns={columns} dataSource={data} />
+        <>
+            <Table
+                columns={columns}
+                dataSource={dataUsers}
+                rowKey={"_id"}
+            />
+
+            <UpdateUserModal
+                isModalUpdateOpen={isModalUpdateOpen}
+                setIsModalUpdateOpen={setIsModalUpdateOpen}
+                dataUpdate={dataUpdate}
+                setDataUpdate={setDataUpdate}
+                loadUser={loadUser}
+            />
+
+            <ViewUserDetail
+                isDetailOpen={isDetailOpen}
+                setIsDetailOpen={setIsDetailOpen}
+                dataDetail={dataDetail}
+                setDataDetail={setDataDetail}
+            />
+        </>
     )
 }
 
---
+++ src/components/user/view.user.detail.jsx
@@ -0,0 +1,38 @@
+import { Drawer } from "antd";
+
+const ViewUserDetail = (props) => {
+    const {
+        dataDetail,
+        setDataDetail,
+        isDetailOpen,
+        setIsDetailOpen
+    } = props;
+
+    return (
+        <Drawer
+            title="Chi tiết User"
+            closable={{ 'aria-label': 'Close Button' }}
+            onClose={() => {
+                setDataDetail(null);
+                setIsDetailOpen(false);
+            }}
+            open={isDetailOpen}
+        >
+            {dataDetail ? <>
+                <p>Id: {dataDetail._id}</p>
+                <br />
+                <p>Full Name: {dataDetail.fullName}</p>
+                <br />
+                <p>Email: {dataDetail.email}</p>
+                <br />
+                <p>Phone Number: {dataDetail.phone}</p>
+            </> :
+                <>
+                    <p>Không tìm thấy dữ liệu</p>
+                </>}
+
+        </Drawer>
+    )
+}
+
+export default ViewUserDetail;
